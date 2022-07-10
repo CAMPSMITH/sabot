d8e2b050cda08036734bc14a848162adba0767c5
Martin Smith <msmith92663@gmail.com>
2022-07-10 00:24:30 -0700

    command line model tester

--- utils/utils.py
+++ utils/utils.py
@@ -240,12 +240,3 @@ def get_historical_prices(currency,
                                          volume_to)
     return df_historical_price
 
-
-
-def make_label(row, current_price_col, future_price_col, offset_col):
-    if row[current_price_col] - row[future_price_col] > row[offset_col]:
-        return -1
-    elif row[future_price_col] - row[current_price_col] > row[offset_col]:
-        return 1
-    else:
-        return 0
