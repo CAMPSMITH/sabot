984fecb643b2c17f45577d42e6d956108906efdf
Martin Smith <msmith92663@gmail.com>
2022-07-10 11:47:43 -0700

    Logistic regression results

--- evaluate_model.py
+++ evaluate_model.py
@@ -16,7 +16,13 @@ suported_models = ['SVC',
                         
 def run(model_name):
     assert model_name in suported_models, f"{model_name} is not valid"
-    evaluate_model(model_name)
+    evaluate_model(    
+        model_name="LogisticRegression", 
+        short_window_sizes = [1,2,3,4,6],
+        long_window_sizes = [24,42,60,180,252],
+        periods=[1],
+        txn_maxs=[20000,30000,100000],
+        factors=[0.1,0.25,0.5,0.8,1,2,3])
 
 if __name__ == "__main__":
     fire.Fire(run)
