40a2b807285135378dc70153ffccbf66e3ef29bb
Martin Smith <msmith92663@gmail.com>
2022-07-12 14:30:25 -0700

    fee and mc analysis

--- evaluate_model.py
+++ evaluate_model.py
@@ -25,24 +25,20 @@ def run(model_name):
 
     evaluate_model(   
         model_name=model_name, 
-        short_window_sizes = [1,2,4,6],
-        long_window_sizes = [24,42,60,180],
-        periods=[1,2,4,6],
-        txn_maxs=[20000,30000,100000],
-        factors=[0.1,0.25,0.5,0.8],
-        training_dataset_months=[1,3,6],
-        # short_window_sizes = [2],
-        # long_window_sizes = [42],
-        # periods=[1],
-        # txn_maxs=[100000],
-        # factors=[0.5],
+        short_window_sizes = [4],
+        long_window_sizes = [24],
+        periods=[1],
+        txn_maxs=[100000],
+        factors=[0.25],
+        training_dataset_months=[6],
         save_results=True,
         save_figure=True,
         show_legend=False,
         export=False,
-        save_file_prefix='',
+        save_file_prefix='mc_p2_',
         fee_pcts=[0.0004],
-        fee_flats=[14]
+        fee_flats=[14],
+        trials=300
     )
 
 if __name__ == "__main__":
